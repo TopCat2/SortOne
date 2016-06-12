aa1d03f8d715a1b7c2aee7bf152105435b4e0b4c
Peter Lynn <github@fantutte.net>
2016-06-12 12:05:40 -0400

    Checkpoint - full statistics without global variables

--- quicksort.js
+++ quicksort.js
@@ -1,12 +1,14 @@
-var compsP, compsS, swaps, callsP, callsI, rangeP;
+
 
 function quickSort(arr) {
-    compsP = 0;
-    compsS=0;
-    swaps = 0;
-    callsP = 0;
-    callsI = 0;
-    rangeP = 0;
+    var compsP, compsS, swaps, callsP, callsI, rangeP;
+    compsP = 0; // Comparisons done in Partition loop
+    compsS = 0; // Comparisons done in Scanner loop
+    swaps = 0;  // Swap calls
+    callsP = 0; // Calls to Partition
+    callsI = 0; // Calls to inner Quicksort
+    rangeP = 0; // Range of numbers partitioned
+    
     var result = quickInner(arr, 0, arr.length - 1);
     console.log("quick sorting", arr.length.toLocaleString(), "numbers took",
         compsS.toLocaleString(), "scanner comparisons, " +
@@ -16,60 +18,59 @@ function quickSort(arr) {
     console.log("Total comparisons:", (compsP + compsS).toLocaleString());
 
     return result;
-}
-
-function quickInner(arr, low, high)  {
-    callsI++;
-	if (low >= high) return;
-
-    // Partition array.  Find p = where pivotVal ends up.
-    var p = partition(arr, low, high);
-    quickInner(arr, low, p-1);
-    quickInner(arr, p+1, high);
-
-	return arr;
-}
 
 
+    function quickInner(arr, low, high) {
+        callsI++;
+        if (low >= high) return;
 
-function partition (arr, low, high) {
-var rangeBefore = rangeP;
-var compsBefore = compsP + compsS;
-    callsP++;
-    rangeP += high - low + 1;
-    if (low >= high) return;
-    var where = Math.floor((Math.random() * (high - low + 1)) + low);
-    var pivot = arr[where];
-    swap (arr, where, high);
-    var fng = pushFng(arr, low, high, pivot); // Pointer to First Known Good item, or top if not yet moved.
+        // Partition array.  Find p = where pivotVal ends up.
+        var p = partition(arr, low, high);
+        quickInner(arr, low, p - 1);
+        quickInner(arr, p + 1, high);
 
-    for (var index = low; index < fng; index++) {
-        compsP++;
-        if (arr[index] > pivot) {
-            swap(arr, index, fng - 1);
-            fng = pushFng(arr, low, fng - 1, pivot);
+        return arr;
+    }
+
+
+    function partition(arr, low, high) {
+        var rangeBefore = rangeP;
+        var compsBefore = compsP + compsS;
+        callsP++;
+        rangeP += high - low + 1;
+        if (low >= high) return;
+        var where = Math.floor((Math.random() * (high - low + 1)) + low);
+        var pivot = arr[where];
+        swap(arr, where, high);
+        var fng = pushFng(arr, low, high, pivot); // Pointer to First Known Good item, or top if not yet moved.
+
+        for (var index = low; index < fng; index++) {
+            compsP++;
+            if (arr[index] > pivot) {
+                swap(arr, index, fng - 1);
+                fng = pushFng(arr, low, fng - 1, pivot);
+            }
+        }
+
+        swap(arr, high, index);
+        return index;
+    }
+
+    function pushFng(arr, low, now, pivot) {
+        for (var i = now; i > low; i--) {
+            compsS++;
+            if (arr[i - 1] < pivot) return i;
+        }
+        return low;
+    }
+
+    function swap(arr, a, b) {
+        swaps++
+        if (a != b) {
+            var temp = arr[a]
+            arr[a] = arr[b];
+            arr[b] = temp;
         }
     }
-
-    swap(arr, high, index);
-    return index;
 }
-
-function pushFng(arr, low, now, pivot) {
-    for (var i = now; i > low; i--) {
-        compsS++;
-        if (arr[i - 1] < pivot) return i;
-    }
-    return low;
-}
-
-function swap (arr, a, b) {
-    swaps++
-    if (a != b) {
-        var temp = arr[a]
-        arr[a] = arr[b];
-        arr[b] = temp;
-    }
-}
-
 module.exports = quickSort;
